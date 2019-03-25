ce28c812b56b77a0ee84a528edd1b4f566a04c80
STNKdevelopment <aleksandr.stnk@gmail.com>
2019-03-25 16:12:33 +0300

    Мелкие правки настроек Spring Session

--- src/main/java/ru/stnk/RestTestAPI/RestTestAPIApplication.java
+++ src/main/java/ru/stnk/RestTestAPI/RestTestAPIApplication.java
@@ -2,21 +2,11 @@ package ru.stnk.RestTestAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
-import org.springframework.session.web.http.HttpSessionStrategy;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableJdbcHttpSession
 public class RestTestAPIApplication {
-
-	@Bean
-	public HttpSessionStrategy httpSessionStrategy() {
-		return new HeaderHttpSessionStrategy();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestTestAPIApplication.class, args);
