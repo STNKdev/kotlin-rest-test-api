3c593d8ade2275607e98efcd85bcf200b99a15d2
STNKdevelopment <aleksandr.stnk@gmail.com>
2019-05-30 14:58:55 +0300

    ""

--- src/main/java/ru/stnk/RestTestAPI/configuration/SessionConfig.java
+++ src/main/java/ru/stnk/RestTestAPI/configuration/SessionConfig.java
@@ -1,10 +1,7 @@
 package ru.stnk.RestTestAPI.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 import org.springframework.session.web.http.CookieHttpSessionIdResolver;
